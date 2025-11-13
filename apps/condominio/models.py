c08606ba414a1b4b9e1eab7132287d422e42be4b
LuisaWentz57 <luisa.wentz.estudo@gmail.com>
2025-11-13 20:24:11 -0300

    Criar classes do condominio

--- apps/condominio/models.py
+++ apps/condominio/models.py
@@ -2,3 +2,16 @@ from django.db import models
 from condominio.models import Condominio
 
 # Create your models here.
+class Condominio(models.Model):
+    nome = models.CharField('Nome', max_length=50)
+    endereco = models.CharField('Endereco', max_length=100)
+    quantidade_blocos = models.IntegerField('Quantidade de Blocos')
+    ativo = models.BooleanField('Ativo', default=True)
+        
+    class Meta:
+        verbose_name = 'Condominio'
+        verbose_name_plural = 'Condominios'
+        ordering =['id']
+
+    def __str__(self):
+        return self.nome
