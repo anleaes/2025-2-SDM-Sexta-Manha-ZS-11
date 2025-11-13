6e9657c4d93cbcd7f21390ee43562ed083363685
LuisaWentz57 <luisa.wentz.estudo@gmail.com>
2025-11-13 20:35:13 -0300

    Criar classes do condominio

--- apps/condominio/serializer.py
+++ apps/condominio/serializer.py
@@ -1,2 +1,7 @@
 from .models import Condominio
 from rest_framework import serializers
+
+class CondominioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Condominio
+        fields = '__all__'
