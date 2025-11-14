1f2a0342ee45474f728f420c203b401359a5a705
LuisaWentz57 <luisa.wentz.estudo@gmail.com>
2025-11-13 21:09:17 -0300

    Configurar urls projeto

--- apps/condominio/urls.py
+++ apps/condominio/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 from rest_framework import routers
 
-app_name = 'condominios'
+app_name = 'condominio'
 
 router = routers.DefaultRouter()
 router.register('', views.CondominioViewSet, basename='condominios')
