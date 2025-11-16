6c3cc4e6b1cb5a2a96a50a8b4165edd0816d1f13
LuisaWentz57 <luisa.wentz.estudo@gmail.com>
2025-11-16 16:06:01 -0300

    Criar classes do views do visitante

--- apps/visitantes/views.py
+++ apps/visitantes/views.py
@@ -3,3 +3,6 @@ from .models import Visitante
 from .serializer import VisitanteSerializer
 
 # Create your views here.
+class VisitanteViewSet(viewsets.ModelViewSet):
+    queryset = Visitante.objects.all()
+    serializer_class = VisitanteSerializer
