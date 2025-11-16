134090048d9083ab8dabdbf2bc786e735eb7acf5
LuisaWentz57 <luisa.wentz.estudo@gmail.com>
2025-11-16 16:33:12 -0300

    Configurar urls do projeto condominioapp

--- condominioapp/urls.py
+++ condominioapp/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('condominios/', include('condominios.urls', namespace='condominios') ),
+    path('visitantes/', include('visitantes.urls', namespace='visitantes')),
 ]
