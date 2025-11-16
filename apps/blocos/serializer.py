e62e499085f1ab4f9ba9eb9a053f0f4786709e92
LuisaWentz57 <luisa.wentz.estudo@gmail.com>
2025-11-16 18:58:33 -0300

    Criar classes do serializer do blocos

--- apps/blocos/serializer.py
+++ apps/blocos/serializer.py
@@ -1,2 +1,7 @@
 from .models import Bloco
 from rest_framework import serializers
+
+class BlocoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bloco
+        fields = '__all__'
