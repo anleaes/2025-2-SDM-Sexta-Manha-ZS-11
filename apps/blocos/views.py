17bbd4a115470194dadfe86bac355760620e7ffe
LuisaWentz57 <luisa.wentz.estudo@gmail.com>
2025-11-16 19:39:14 -0300

    Criar classes do views do blocos

--- apps/blocos/views.py
+++ apps/blocos/views.py
@@ -3,3 +3,6 @@ from rest_framework import viewsets
 from .serializer import BlocoSerializer
 
 # Create your views here.
+class BlocoViewSet(viewsets.ModelViewSet):
+    queryset = Bloco.objects.all()
+    serializer_class = BlocoSerializer
