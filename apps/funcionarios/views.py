955eb7ad0adf3c0ba998a66af2316c99805a763f
LuisaWentz57 <luisa.wentz.estudo@gmail.com>
2025-11-16 22:20:57 -0300

    Criar classes do views do funcionario

--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -3,3 +3,6 @@ from rest_framework import viewsets
 from .serializer import FuncionarioSerializer
 
 # Create your views here.
+class FuncionarioViewSet(viewsets.ModelViewSet):
+    queryset = Funcionario.objects.all()
+    serializer_class = FuncionarioSerializer 
