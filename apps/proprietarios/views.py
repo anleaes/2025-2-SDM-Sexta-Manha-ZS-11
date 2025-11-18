08e9dcacf300b1010c2fff3da20ced719beb8cd4
LuisaWentz57 <luisa.wentz.estudo@gmail.com>
2025-11-18 17:00:09 -0300

    Criar views do proprietarios

--- apps/proprietarios/views.py
+++ apps/proprietarios/views.py
@@ -3,3 +3,6 @@ from .models import Proprietario
 from .serializer import ProprietarioSerializer
 
 # Create your views here.
+class ProprietarioViewSet(viewsets.ModelViewSet):
+    queryset = Proprietario.objects.all()
+    serializer_class = ProprietarioSerializer
