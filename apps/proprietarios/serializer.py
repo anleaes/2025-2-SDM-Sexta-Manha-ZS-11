3a1df2d47bca9a5c2deef01dfa844beae03fc817
LuisaWentz57 <luisa.wentz.estudo@gmail.com>
2025-11-18 16:53:47 -0300

    Criar classes do serializer do proprietarios

--- apps/proprietarios/serializer.py
+++ apps/proprietarios/serializer.py
@@ -1,2 +1,7 @@
 from .models import Proprietario
 from rest_framework import serializers
+
+class ProprietarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Proprietario
+        fields = '__all__'
