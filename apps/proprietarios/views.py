2da8502a962da4165d6bfa1a5ea989b14d2687c0
LuisaWentz57 <luisa.wentz.estudo@gmail.com>
2025-11-18 16:58:19 -0300

    Criar imports do views do proprietarios

--- apps/proprietarios/views.py
+++ apps/proprietarios/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Proprietario
+from .serializer import ProprietarioSerializer
 
 # Create your views here.
