36277fe68fcd41c2825ec0e504f7e6c3f69d3e6b
LuisaWentz57 <luisa.wentz.estudo@gmail.com>
2025-11-18 16:45:17 -0300

    Criar class do models do proprietario

--- apps/proprietarios/models.py
+++ apps/proprietarios/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Proprietario(models.Model):
+    nome = models.CharField('Nome', max_length=50)
+    email = models.CharField('Email', max_length=50)
+    telefone = models.CharField('Telefone', max_length=15)
+    data_entrada = models.DateField('Data de Entrada')
+    
