41456cb9badef6d82cf38ac3deaaf9d68e5a495d
LuisaWentz57 <luisa.wentz.estudo@gmail.com>
2025-11-19 16:48:03 -0300

    Criar imports do models do visitas

--- apps/visitas/models.py
+++ apps/visitas/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from visitantes.models import Visitante
+from apartamentos.models import Apartamento
+from proprietarios.models import Proprietario
 
 # Create your models here.
