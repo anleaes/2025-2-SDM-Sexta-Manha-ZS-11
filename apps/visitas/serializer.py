d45cdec4ca57f16dccd018b5ec631af6f70116c6
LuisaWentz57 <luisa.wentz.estudo@gmail.com>
2025-11-19 16:56:40 -0300

    Criar classes do serializer do visitas

--- apps/visitas/serializer.py
+++ apps/visitas/serializer.py
@@ -1,2 +1,7 @@
 from .models import Visita
 from rest_framework import serializers
+
+class VisitaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Visita
+        fields = '__all__'
