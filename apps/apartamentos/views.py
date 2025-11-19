1b7c845dc983c94d3baaaca83292dbe3696ec98d
LuisaWentz57 <luisa.wentz.estudo@gmail.com>
2025-11-19 15:05:27 -0300

    Criar views do apartamentos

--- apps/apartamentos/views.py
+++ apps/apartamentos/views.py
@@ -3,3 +3,6 @@ from rest_framework import viewsets
 from .serializer import ApartamentoSerializer
 
 # Create your views here.
+class ApartamentoViewSet(viewsets.ModelViewSet):
+    queryset = Apartamento.objects.all()
+    serializer_class = ApartamentoSerializer
