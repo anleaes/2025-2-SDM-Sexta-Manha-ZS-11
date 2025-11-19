cc323a34c71635f7aca4cbc35c27d5525cba3721
LuisaWentz57 <luisa.wentz.estudo@gmail.com>
2025-11-19 14:32:32 -0300

    Criar classes do serializer do apartamento

--- apps/apartamentos/serializer.py
+++ apps/apartamentos/serializer.py
@@ -1,2 +1,7 @@
 from .models import Apartamento
 from rest_framework import serializers
+
+class ApartamentoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Apartamento
+        fields = '__all__'
