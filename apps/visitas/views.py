e4547538d4490ffcbc78342fe519661fc23ddee3
LuisaWentz57 <luisa.wentz.estudo@gmail.com>
2025-11-19 16:59:15 -0300

    Criar views do app visitas

--- apps/visitas/views.py
+++ apps/visitas/views.py
@@ -3,3 +3,6 @@ from rest_framework import viewsets
 from .serializer import VisitaSerializer
 
 # Create your views here.
+class VisitaViewSet(viewsets.ModelViewSet):
+    queryset = Visita.objects.all()
+    serializer_class = VisitaSerializer
